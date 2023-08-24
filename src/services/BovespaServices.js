3a27a1fea85b018613760a0c89baeffa82336357
João Victor <joao.victor.valentim@hotmail.com>
2023-08-23 22:58:22 -0300

    Adicionando Token bovespa

--- src/services/BovespaServices.js
+++ src/services/BovespaServices.js
@@ -6,7 +6,7 @@ const bovespa = require("bovespa")();
 const API_URL = "https://brapi.dev/api/quote/"
 
  const getPrice = async code => {
-  const url = `${API_URL}${code}`
+  const url = `${API_URL}${code}?token=4W2JRvyjDqRCe4miXnhgpL`
   const resolver = data => format(data.results[0].regularMarketPrice, SYMBOL_BRL)
   const price = await doGetRequest(url, {}, resolver)
   return { code, price }
